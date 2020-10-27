987d25b3877cc6bbc1f32f66fbe8d11991a4b8f1
arcsinY <34400130+arcsinY@users.noreply.github.com>
2020-10-27 19:46:24 +0800

    Add files via upload

---
+++ 哈希/49.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int[] alphabet = new int[26];  //每个单词的字母表
+        Map<int[], Integer> alp_int = new HashMap<>();  //记录字母表和对应数组的index
+        ArrayList<List<String>> res = new ArrayList<List<String>>();
+        for(int i=0;i<strs.length;++i)
+        {
+            Arrays.fill(alphabet, 0);
+            //计算单词i的字母表
+            for(int j=0;j<strs[i].length();++j)
+            {
+                char t = strs[i].charAt(j);
+                alphabet[t-'a']++;
+            }
+            //int j;
+            boolean have=false;
+            for(int[] j:alp_int.keySet())
+            {
+                if(Arrays.equals(j,alphabet))   //这个字母排列出现过
+                {
+                    int t = alp_int.get(j);
+                    res.get(t).add(strs[i]);
+                    have=true;
+                    break;
+                }
+
+            }
+            if(have == false)  //这个字母排列没出现过
+            {
+                int[] alphabet2 = new int[26];
+                alphabet2 =  Arrays.copyOf(alphabet, 26);
+                alp_int.put(alphabet2, alp_int.size());
+                ArrayList<String> tt = new ArrayList<>();
+                tt.add(strs[i]);
+                res.add(tt);
+            }
+        }
+        return res;
+    }
+}
