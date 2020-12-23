5f5b9d53fa5e931700d1aa60dbf80ffd9bf71b7a
arcsinY <34400130+arcsinY@users.noreply.github.com>
2020-12-23 21:19:20 +0800

    Update 79.java

--- 搜索/79.java
+++ 搜索/79.java
@@ -1,40 +1,48 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int h = board.length, w = board[0].length;
-        boolean[][] visited = new boolean[h][w];
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                boolean flag = check(board, visited, i, j, word, 0);
-                if (flag) {
-                    return true;
+        if(board == null || word == null)
+            return false;
+        int m=board.length;int n = board[0].length;
+        boolean[][] visit = new boolean[m+2][n+2];
+        for(int i=0;i<m;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                if(board[i][j] == word.charAt(0))
+                {
+                    if (dfs(board, word, m, n, visit, 0, i, j))
+                        return true;
+                    for(int k=0;k<=m;++k)
+                    {
+                        Arrays.fill(visit[k],false);
+                    }
                 }
             }
         }
         return false;
     }
-
-    public boolean check(char[][] board, boolean[][] visited, int i, int j, String s, int k) {
-        if (board[i][j] != s.charAt(k)) {
-            return false;
-        } else if (k == s.length() - 1) {
+    public boolean dfs(char[][] board, String word, int m, int n, boolean[][] visit, int start, int row, int col)
+    {
+        if(start == word.length())
             return true;
-        }
-        visited[i][j] = true;
-        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        boolean result = false;
-        for (int[] dir : directions) {
-            int newi = i + dir[0], newj = j + dir[1];
-            if (newi >= 0 && newi < board.length && newj >= 0 && newj < board[0].length) {
-                if (!visited[newi][newj]) {
-                    boolean flag = check(board, visited, newi, newj, s, k + 1);
-                    if (flag) {
-                        result = true;
-                        break;
-                    }
-                }
-            }
-        }
-        visited[i][j] = false;
-        return result;
+        if(row <0 || row>=m || col<0 || col>=n)
+            return false;
+        if(word.charAt(start) != board[row][col])
+            return false;
+        if(visit[row][col])
+            return false;
+        visit[row][col] = true;
+        if(dfs(board, word, m,n,visit, start+1,row-1, col))
+            return true;
+        if(dfs(board, word, m,n,visit, start+1,row+1, col))
+            return true;
+        if(dfs(board, word, m,n,visit, start+1,row, col-1))
+            return true;
+        if(dfs(board, word, m,n,visit, start+1,row, col+1))
+            return true;
+        visit[row][col]=false;
+        return false;
     }
 }
