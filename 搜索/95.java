657a1744498c21cb365aecca3fd0f9d70734c9c4
arcsinY <34400130+arcsinY@users.noreply.github.com>
2021-01-11 21:22:08 +0800

    Update 95.java

--- 搜索/95.java
+++ 搜索/95.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 // 对start~end的数组建立排序二叉树，选定一个数字i为根，递归对start~(i-1)建立左子树，对(i+1)~end建立右子树
-// 特别注意递归出口是什么，什么时候加入null
+// 特别注意递归出口是什么，要执行什么操作
 class Solution {
     public List<TreeNode> generateTrees(int n) {
         if(n == 0) {
@@ -9,15 +9,15 @@ class Solution {
         }
         return dfs(1,n);
     }
-    // 返回的是根节点列表
+    // 返回的是根节点列表。每一层递归的操作得到的是一棵树，将树根加入到res中
     public List<TreeNode> dfs(int start, int end) {
         List<TreeNode> res = new ArrayList<>();
-        // 递归出口
+        // 递归出口，这种情况下得不到树，根为null，将null加入到res中
         if(start > end){
             res.add(null);
             return res;
         }
-        // 遍历所有根节点
+        // 遍历所有根节点，每次循环得到一颗树，树根为i，将树根加入res中
         for(int i = start; i <= end; ++i) {
             List<TreeNode> left = dfs(start, i - 1);
             List<TreeNode> right = dfs(i + 1, end);
