e6d446414aff871e05681af7c4cd57f92c4eea6e
arcsinY <34400130+arcsinY@users.noreply.github.com>
2021-04-11 20:46:08 +0800

    Create 32.java

---
+++ 动态规划/32.java
@@ -0,0 +1,36 @@
+// dp[i]：以s[i]为结尾的最长括号子串
+// 遍历到i时，考虑s[i-1]和s[i]两个字符，只有s[i]==')'时才可能出现以s[i]为结尾的括号串
+// 因此有两种情况：s[i-1]=='(', s[i-1]==s[i]==')'
+// 对于第一种情况，相比于s[i-2]为结尾的最长括号串多了一对括号，因此dp[i]=dp[i-2]+1
+// 对于第二种情况，寻找以i-1为结尾的最长括号串的前一个字符，若为'('，说明相比于以i-1为结尾的最长括号串多了一对括号，同时要加上更之前的最长括号串
+class Solution {
+    public int longestValidParentheses(String s) {
+        if (s.length() == 0 || s.length() == 1) {
+            return 0;
+        }
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); ++i) {
+            if (s.charAt(i) == ')' && s.charAt(i-1) == '(') {
+                if (i >= 2) {
+                    dp[i] = dp[i - 2] + 2;
+                } else {
+                    dp[i] = 2;
+                }
+            } else if (s.charAt(i) == ')' && s.charAt(i - 1) == ')' && (i-dp[i-1]-1) >= 0 && s.charAt(i-dp[i-1]-1) == '(') {   // i-dp[i-1]-1是以i-1为结尾的最长括号串的前一个位置
+                if ((i-dp[i-1]-2) >= 0) {
+                    // dp[i - dp[i - 1] - 2]是更之前的一个括号串
+                    dp[i] = dp[i - 1] + dp[i - dp[i - 1] - 2] + 2;
+                } else {
+                    dp[i] = dp[i - 1] + 2;
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (dp[i] > res) {
+                res = dp[i];
+            }
+        }
+        return res;
+    }
+}
