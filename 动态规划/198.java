8fd176c06f4812eb13458b20ab8756258e3a615a
arcsinY <34400130+arcsinY@users.noreply.github.com>
2021-04-14 19:21:09 +0800

    Update 198.java

--- 动态规划/198.java
+++ 动态规划/198.java
@@ -1,27 +1,12 @@
-// dp[i]：最后抢第i家的最大收益
-// 最终结果要返回dp中最大的
+// dp[i]：从第i家开始抢得到的收益
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 1) {
-            return nums[0];
+        int[] dp = new int[nums.length + 2];
+        for (int i = nums.length - 1; i >= 0; --i) {
+            // 1. 放弃当前房子，从下一家开始抢
+            // 2. 抢当前房子，放弃下一家，从下下家开始抢
+            dp[i] = Math.max(dp[i + 1], nums[i] + dp[i + 2]);
         }
-        int[] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(dp[0], nums[1]);
-        int res = Math.max(dp[0], dp[1]);
-        for (int i = 2; i < n; ++i) {
-            int max = Integer.MIN_VALUE;
-            for (int j = 0; j < i - 1; ++j) {
-                if (dp[j] > max) {
-                    max = dp[j];
-                }
-            }
-            dp[i] = max + nums[i];
-            if (dp[i] > res) {
-                res = dp[i];
-            }
-        }
-        return res;
+        return dp[0];
     }
 }
