85c65210a2c30c091db65185bc2089f3a6e0d0b9
arcsinY <34400130+arcsinY@users.noreply.github.com>
2021-07-26 19:32:04 +0800

    Update 300.java

--- 动态规划/300.java
+++ 动态规划/300.java
@@ -1,20 +1,36 @@
-// 状态：dp[i]：以i为结尾得最长上升子序列
-// 选择：第i个数字可以拼接在前面任何一个子序列之后
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        int res = 1;
-        for (int i = 1; i < nums.length; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-            if (dp[i] > res) {
-                res = dp[i];
-            }
-        }
-        return res;
-    }
+// 因为要子序列最长，子序列的增加过程越慢越好
+// ans[i]：长度为i的最长递增子序列最后一位数字。ans一定是递增的，因此可以使用二分搜索
+// 遍历nums，若nums[j] > ans中的最后一个数字，则加入ans末尾，意思是在长度为len - 1的子序列上加了一个数字，得到长度为len的子序列
+// 否则找出当前ans中的位置i，满足ans[i-1] < nums[j] < ans[i]，用nums[j]替换ans[i]。意思是在长度为i - 1的子序列后面加上一个更小的数字，得到长度为i的子序列
+func lengthOfLIS(nums []int) int {
+	ans := make([]int, len(nums) + 1)
+	index := 1
+	ans[index] = nums[0]
+	for i := range nums {
+		if nums[i] > ans[index] {
+			ans[index + 1] = nums[i]
+			index++
+		} else {
+			t := binarySearch(ans, nums[i], index)
+			ans[t] = nums[i]
+		}
+	}
+	return index
+}
+
+// 在ans中寻找 < target的最大位置i，因为要修改的是ans[i - 1]，因此返回i-1
+func binarySearch(ans []int, target int, index int) int {
+	low := 1
+	high := index
+	for low <= high {
+		mid := low + (high - low) / 2
+		if ans[mid] < target {
+			low = mid + 1
+		} else if ans[mid] == target{
+			return mid
+		} else {
+			high = mid - 1
+		}
+	}
+	return high + 1
 }
