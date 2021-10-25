b7504208aa343e8952e72b0dffa65448d957361a
arcsinY <34400130+arcsinY@users.noreply.github.com>
2021-10-25 12:24:13 +0800

    Update 200.java

--- 搜索/200.java
+++ 搜索/200.java
@@ -1,39 +1,28 @@
-// 从所有的1开始dfs，用vis记录遍历过的1
+// 从所有的1开始dfs，遍历到的地方改为0，避免额外使用vis记录
 // 一次dfs的结果是一个岛屿
 class Solution {
+    int[][] dir = new int[][]{{0,1}, {0,-1}, {1,0}, {-1,0}};
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int res = 0;
-        boolean[][] vis = new boolean[m][n];
+        int ans = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (vis[i][j] || grid[i][j] == '0') {
-                    continue;
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j, m, n);
+                    ++ans;
                 }
-                dfs(grid, i, j, vis, m, n);
-                ++res;
             }
         }
-        return res;
+        return ans;
     }
-    public void dfs(char[][] grid, int i, int j, boolean[][] vis, int m, int n) {
-        // 遍历到搜索过的，或0则停止
-        if (vis[i][j] == true || grid[i][j] == '0') {
+    public void dfs(char[][] grid, int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') {
             return;
         }
-        vis[i][j] = true;
-        if (i != 0) {
-            dfs(grid, i - 1, j, vis, m, n);
-        }
-        if (i != m - 1) {
-            dfs(grid, i + 1, j, vis, m, n);
-        }
-        if (j != 0) {
-            dfs(grid, i, j - 1, vis, m, n);
-        }
-        if (j != n - 1) {
-            dfs(grid, i, j + 1, vis, m, n);
+        grid[i][j] = '0';
+        for (int[] d : dir) {
+            dfs(grid, i  + d[0], j + d[1], m, n);
         }
     }
 }
