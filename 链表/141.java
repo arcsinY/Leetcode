4bca219fb211979b57addfd082c14e240f239c9a
arcsinY <34400130+arcsinY@users.noreply.github.com>
2021-11-30 11:14:18 +0800

    Update 141.java

--- 链表/141.java
+++ 链表/141.java
@@ -2,18 +2,14 @@
 // 弗洛伊德判圈方法：快慢指针，快指针每次走两步，慢指针一步。若快指针走到结束说明没有环。某一时刻快指针 == 慢指针说明有环（套了一圈）
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return false;
-        }
-        ListNode p1 = head;
-        ListNode p2 = head.next;
-        while (p1 != p2) {
-            if (p2 == null || p2.next == null) {
-                return false;
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
             }
-            p1 = p1.next;
-            p2 = p2.next.next;
         }
-        return true;
+        return false;
     }
 }
