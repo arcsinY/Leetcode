e253e9da8978300f5ace8fb617ee8b49d3039601
arcsinY <34400130+arcsinY@users.noreply.github.com>
2021-12-22 10:20:24 +0800

    Update 10.java

--- 动态规划/10.java
+++ 动态规划/10.java
@@ -1,23 +1,25 @@
-// 状态：dp[i][j]：s的前i个字符和p的前j个字符能否匹配
-// 选择：p的第j个字符不是*，则p的第j个字符和s的第i个字符匹配
-//      p的第j个字符是*，则p的第j和j-1个字符可以不和s匹配。若p的j-1个字符和s的第i个字符匹配，则这个字符可以和s前面的字符继续匹配
-class Solution {
-    public boolean isMatch(String s, String p) {
-        int m = s.length();
-        int n = p.length();
+// 状态：dp[i][j]：s 的前 i 个字符和 p 的前 j 个字符能否匹配
+// 选择：
+// (1) p 的第 j 个字符不是 * ，则 p 的第 j 个字符必须和 s 的第 i 个字符匹配
+// (2) p 的第 j 个字符是 * ，则：
+//     (2-1) p 的第 j-1 个字符不和 s 的第 i 个字符匹配，直接忽略 p 的第 j 和 j-1 个字符
+//     (2-2) p 的第 j-1 个字符和 s 的第 i 个字符匹配，同时可以和 s 前面的字符继续匹配
+public class Solution {
+    public boolean match (String str, String pattern) {
+        int m = str.length();
+        int n = pattern.length();
         boolean[][] dp = new boolean[m + 1][n + 1];
         dp[0][0] = true;
-        for (int i = 0; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                if (p.charAt(j - 1) == '*') {
-                    // p的第j-1和j个字符不和s匹配
-                    dp[i][j] = dp[i][j - 2];
-                    if (match(s, p, i, j - 1)) {
-                        // p的第j-1个字符可以继续和前面的匹配
-                        dp[i][j] = dp[i][j] || dp[i - 2][j - 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (pattern.charAt(j - 1) == '*') {
+                    if (chMatch(str, pattern, i, j - 1)) {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
                     }
                 } else {
-                    if (match(s, p, i, j)) {
+                    if (chMatch(str, pattern, i, j)) {
                         dp[i][j] = dp[i - 1][j - 1];
                     }
                 }
@@ -25,14 +27,13 @@ class Solution {
         }
         return dp[m][n];
     }
-    // s的第i个字符能否和p的第j个字符匹配
-    public boolean match(String s, String p, int i, int j) {
+    public boolean chMatch(String s, String p, int i, int j) {
         if (i == 0) {
             return false;
         }
-        if (p.charAt(j - 1) == '.') {
+        if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
             return true;
         }
-        return s.charAt(i - 1) == p.charAt(j - 1);
+        return false;
     }
 }
