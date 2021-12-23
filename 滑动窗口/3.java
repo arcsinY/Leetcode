ac465c057fd09e0f828701f2ffc5217157da210f
arcsinY <34400130+arcsinY@users.noreply.github.com>
2021-12-23 10:57:13 +0800

    Update 3.java

--- 滑动窗口/3.java
+++ 滑动窗口/3.java
@@ -1,22 +1,22 @@
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> window = new HashMap<>();     // 窗口内各个字母的个数，保证窗口内所有字母都只出现一次
+// 判断[left, right)的字串是否有重复字符
+public class Solution {
+    public int lengthOfLongestSubstring (String s) {
+        int ans = 0;
         int left = 0, right = 0;
-        int res = 0;
+        // 记录出现过的字符
+        Set<Character> set = new HashSet<>();
+        // 由于right是开区间，因此最大可以另right == s.length()
         while (right < s.length()) {
             char c = s.charAt(right);
-            ++right;
-            window.put(c, window.getOrDefault(c, 0) + 1);
-            // 原窗口中所有字母都是只出现一次，因此如果窗口内出现了重复字母，一定是新加入的重复了
-            while (window.get(c) > 1) {
-                char t = s.charAt(left);
-                window.put(t, window.get(t) - 1);
-                ++left;
+            right++;
+            // 不允许之前出现过字符c
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
             }
-            if (right - left > res) {
-                res = right - left;
-            }
+            set.add(c);
+            ans = Math.max(ans, right - left);
         }
-        return res;
+        return ans;
     }
 }
