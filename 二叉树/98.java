08975a38c47ee8a5c156a6026ad5a23003bd8286
arcsinY <34400130+arcsinY@users.noreply.github.com>
2021-12-28 13:01:30 +0800

    Update 98.java

--- 二叉树/98.java
+++ 二叉树/98.java
@@ -1,14 +1,14 @@
 // 二叉树的中序遍历，但不需要记录每一个值，只在访问根节点时记录这个值
 // min记录的是已经访问的点的最大值，之后访问的点要比这个更大才符合定义
 class Solution {
-    double min = -Double.MAX_VALUE;  //初始的值，最小数
+    long min = -Long.MAX_VALUE;  //初始的值，最小数
     public boolean isValidBST(TreeNode root) {
         return traverse(root);
     }
     // 判断以root为根的二叉树是不是有效的
     public boolean traverse(TreeNode root) {
         if (root.left != null) {  // 判断左子树是否有效
-            if (traverse(root.left) == false) {  // 无效直接返回false
+            if (!traverse(root.left)) {  // 无效直接返回false
                 return false;
             }
         }
