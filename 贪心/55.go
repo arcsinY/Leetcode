6afb3c78fd08e01f69c029dd5262b9d0e1c6d3f9
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-02-06 21:36:22 +0800

    Update 55.go

--- 贪心/55.go
+++ 贪心/55.go
@@ -1,13 +1,12 @@
 func canJump(nums []int) bool {
     farthest := 0
-    for i := 0; i < len(nums) - 1; i++ {
-        farthest = max(farthest, i + nums[i])
-        if farthest <= i {
-            return false
+    for i := 0; i < len(nums); i++ {
+        if i <= farthest {
+            farthest = max(i + nums[i], farthest)
+            if farthest >= len(nums) - 1 {
+                return true
+            }
         }
-    }
-    if farthest >= len(nums) - 1 {
-        return true
     }
     return false
 }
