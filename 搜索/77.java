b9b3bfda74106bf51c5494c7368771ae8eb63777
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-02-12 17:13:14 +0800

    Update 77.java

--- 搜索/77.java
+++ 搜索/77.java
@@ -3,25 +3,26 @@ import java.util.List;
 
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
         List<Integer> one = new ArrayList<>();
-        dfs(1,n,k,one, res);
-        return res;
+        dfs(1, n, k, one, ans);
+        return ans;
     }
 
-    public void dfs(int start, int n, int k, List<Integer> one, List<List<Integer>> res)
+    public void dfs(int start, int n, int k, List<Integer> one, List<List<Integer>> ans)
     {
-        if(one.size()+(n-start+1)<k)  //所有剩下得元素都加上也不可能得到k个
+        if(one.size() + n - start + 1 < k) {  //所有剩下得元素都加上也不可能得到k个
             return;
+        }
         if(one.size() == k)
         {
             List<Integer> t = new ArrayList<>(one);
-            res.add(t);
+            ans.add(t);
             return;
         }
         one.add(start);   //将start加到结果中
-        dfs(start+1, n,k,one,res);
-        one.remove(one.size()-1);  //不要start
-        dfs(start+1, n,k,one,res);
+        dfs(start + 1, n, k, one, ans);
+        one.remove(one.size() - 1);  //不要start
+        dfs(start + 1, n, k, one, ans);
     }
 }
