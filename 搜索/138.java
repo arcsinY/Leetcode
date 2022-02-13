d018cc0424ab51b68228563d9276fa6796609e2b
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-02-13 23:23:46 +0800

    Update 138.java

--- 搜索/138.java
+++ 搜索/138.java
@@ -1,5 +1,7 @@
 class Solution {
+    // 存储旧节点和新节点的对应关系
     Map<Node, Node> map = new HashMap<>();
+    // 复制旧节点 head， 返回复制后的新节点
     public Node copyRandomList(Node head) {
         if (head == null) {
             return null;
@@ -7,9 +9,12 @@ class Solution {
         if (map.containsKey(head)) {
             return map.get(head);
         }
+        // 复制这个节点的值
         Node node = new Node(head.val);
         map.put(head, node);
+        // 复制 next 节点
         node.next = copyRandomList(head.next);
+        // 复制 random 节点
         node.random = copyRandomList(head.random);
         return node;
     }
