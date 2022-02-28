cff660bc931f728cef4b525c69c63973f835286e
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-02-28 20:07:03 +0800

    Update 61.java

--- 链表/61.java
+++ 链表/61.java
@@ -1,11 +1,3 @@
-class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if(head == null || head.next == null || k == 0)
