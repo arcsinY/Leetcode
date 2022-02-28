d45efc374407a8609308b43692844e37823e9417
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-02-28 20:25:01 +0800

    Update 92.java

--- 链表/92.java
+++ 链表/92.java
@@ -1,10 +1,3 @@
-import java.util.List;
-
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-}
 class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
         if(head == null || n == 1){
@@ -13,25 +6,24 @@ class Solution {
         ListNode pre = new ListNode(-1);
         pre.next = head;
         ListNode cur = head;
-        //cur是第m个节点，pre是m-1个
-        while(m > 1){
-            pre = pre.next;
+        // cur 是第 m 个节点，pre 是 m-1 个
+        for (int i = 0; i < m - 1; i++) {
             cur = cur.next;
-            --m;
-            --n;
+            pre = pre.next;
+            n--;
         }
-        ListNode subHead = pre;   //反转部分的前一个节点，反转之后这个节点连到n
-        ListNode subTail = cur;   //第m个节点，反转之后这个节点连接到n+1上
+        ListNode subHead = pre;   // 反转部分的前一个节点，反转之后这个节点连到 n
+        ListNode subTail = cur;   // 第 m 个节点，反转之后这个节点连接到 n+1 上
         ListNode t = null;
-        while(n > 0){
-            t = cur.next;   //保留cur的下一个，否则指针无法前进
-            cur.next = pre;   //反转cur和pre
-            pre = cur;  //pre前进
-            cur = t;    //cur前进
-            --n;
-        }   //循环结束后cur指向n+1，pre指向n
-        //完成最后两步
-        //subHead不是真正链表中的节点，说明链表前一部分都被反转了，直接令反转后的头部（pre）作为head
+        while (n > 0) {
+            t = cur.next;   // 保留 cur 的下一个，否则指针无法前进
+            cur.next = pre; // 反转 cur 和 pre
+            pre = cur;      // pre 前进
+            cur = t;        // cur 前进
+            n--;
+        }  // 循环结束后 cur 指向 n+1，pre 指向 n
+        // 完成最后两步
+        // subHead 不是真正链表中的节点，说明链表前一部分都被反转了，直接令反转后的头部（pre）作为 head
         if(subHead.val != -1){
             subHead.next = pre;
         } else{
