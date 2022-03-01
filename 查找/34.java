3b5d3fbd5f89a65638261eff72d417a2c58d4596
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-03-01 19:58:51 +0800

    Update 34.java

--- 查找/34.java
+++ 查找/34.java
@@ -4,39 +4,38 @@ class Solution {
         int b = right(nums, target);
         return new int[]{a, b};
     }
-    // 查找区间：左闭右开
     public int left(int[] nums, int target) {
-        int low = 0, high = nums.length;
-        while (low < high) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] > target) {
-                high = mid;
+                high = mid - 1;
             } else if (nums[mid] == target) {
-                high = mid;
+                high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        if (low == nums.length || nums[low] != target) {
+        if (low >= nums.length || nums[low] != target) {
             return -1;
         }
         return low;
     }
     public int right(int[] nums, int target) {
-        int low = 0, high = nums.length;
-        while (low < high) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] > target) {
-                high = mid;
+                high = mid - 1;
             } else if (nums[mid] == target) {
                 low = mid + 1;
             } else {
                 low = mid + 1;
             }
         }
-        if (low == 0 || nums[low - 1] != target) {
+        if (high < 0 || nums[high] != target) {
             return -1;
         }
-        return low - 1;
+        return high;
     }
 }
