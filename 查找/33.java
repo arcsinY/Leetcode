9ff55c8b23c1472a1279ed67a8f8ba3d61702ef2
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-03-01 19:56:01 +0800

    Update 33.java

--- 查找/33.java
+++ 查找/33.java
@@ -1,28 +1,36 @@
+// 将数组分为两部分：[left, mid] 和 [mid + 1, right]，一定有其中一部分是有序的
+// 借助有序的这部分判断是否需要在有序的这部分上搜索
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length==0)
+        if(nums.length == 0) {
             return  -1;
-        int low=0;
-        int high=nums.length-1;
+        }
+        int low = 0;
+        int high = nums.length-1;
         int mid;
-        while(low<=high)
-        {
-            mid=(low+high)/2;
-            if(nums[mid] == target)
+        // [low, high] 区间上搜索
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
                 return mid;
-            if(nums[0]<=nums[mid])  //mid在交换位置之前，mid左侧的都比nums[mid]小，但大于nums[0]
-            {
-                if(nums[0]<=target && target<nums[mid])   //在左侧寻找
-                    high=mid-1;
-                else
-                    low=mid+1;
             }
-            else   //mid在交换位置之后，mid右侧的数有序，都大于nums[mid]但小于nums[nums.length-1]
-            {
-                if(nums[mid]<target && target <=nums[nums.length-1])
-                    low = mid+1;
-                else
-                    high = mid-1;
+            if(nums[0] <= nums[mid]) {
+                // mid 在交换位置之前，mid 左侧的都比 nums[mid] 小，但大于 nums[0]
+                if(nums[0] <= target && target < nums[mid]) {  // 在左侧寻找
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            else {
+                // mid 在交换位置之后，mid 右侧的数有序，都大于 nums[mid] 但小于 nums[nums.length - 1]
+                if(nums[mid] < target && target <= nums[nums.length-1]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
             }
         }
         return -1;
