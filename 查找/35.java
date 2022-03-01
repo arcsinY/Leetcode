f109014d12b0a5c78dc85ea9e83c52fdefdd7173
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-03-01 20:02:38 +0800

    Update 35.java

--- 查找/35.java
+++ 查找/35.java
@@ -1,25 +1,21 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        if(nums[0]>target || nums.length == 0)
-            return 0;
-        if(nums[nums.length-1]<target)
-            return nums.length;
-        int low =  0;
-        int high=nums.length-1;
-        int mid=0;
-        while (low<=high)
-        {
-            mid=(low+high)/2;
-            if(nums[mid] == target)
+        int left = 0, right = nums.length - 1;
+        int mid = 0;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
                 return mid;
-            if(nums[mid]<target)
-                low=mid+1;
-            else
-                high=mid-1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-        if(nums[mid]>target)
+        if (nums[mid] > target) {
             return mid;
-        else
-            return mid+1;
+        } else {
+            return mid + 1;
+        }
     }
 }
