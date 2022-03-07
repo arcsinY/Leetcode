6c98487fce2980e74200e9a10dfdac0a35d6fb16
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-03-07 11:21:26 +0800

    Update 199.java

--- 二叉树/199.java
+++ 二叉树/199.java
@@ -1,27 +1,27 @@
-// 二叉树层次遍历，每一层最又侧的节点就是这一层的结果
+// 二叉树层次遍历，每一层最右侧的节点就是这一层的结果
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         if (root == null) {
             return new ArrayList<>();
         }
-        List<Integer> res = new ArrayList<>();
-        Queue<TreeNode> thisLayer = new LinkedList<>();  // 本层节点
-        Queue<TreeNode> nextLayer = new LinkedList<>();  // 下一层节点
-        thisLayer.add(root);
-        while (thisLayer.isEmpty() == false) {
-            TreeNode t = thisLayer.poll();
-            if (t.left != null) {
-                nextLayer.add(t.left);
-            }
-            if (t.right != null) {
-                nextLayer.add(t.right);
-            }
-            if (thisLayer.isEmpty()) {
-                res.add(t.val);
-                thisLayer = new LinkedList<>(nextLayer);
-                nextLayer.clear();
+        List<Integer> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode t = q.poll();
+                if (t.left != null) {
+                    q.offer(t.left);
+                }
+                if (t.right != null) {
+                    q.offer(t.right);
+                }
+                if (i == size - 1) {
+                    ans.add(t.val);
+                }
             }
         }
-        return res;
+        return ans;
     }
 }
