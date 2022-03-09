387b3bb2fd7bd096ba46b55862d9a87df5a09cd4
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-03-09 20:35:00 +0800

    Update 59.java

--- 搜索/59.java
+++ 搜索/59.java
@@ -1,25 +1,27 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int[][] res = new int[n][n];
-        int t = 1;
-        int left=0,right=n-1,top = 0,bottom = n-1;
-        while(left<=right && top <= bottom)
-        {
-            for(int i=left;i<=right;++i)
-                res[top][i] = t++;
-            for(int i=top+1;i<=bottom;++i)
-                res[i][right]=t++;
-            if(left<right && bottom > top){
-            for(int i=right-1;i>left;--i)
-                res[bottom][i] = t++;
-            for(int i=bottom;i>top;--i)
-                res[i][left]=t++;
+        int[][] ans = new int[n][n];
+        int num = 1, left = 0, right = n - 1, top = 0, bottom = n - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                ans[top][i] = num++;
             }
-            ++left;
-            --right;
-            --bottom;
-            ++top;
+            for (int i = top + 1; i <= bottom; i++) {
+                ans[i][right] = num++;
+            }
+            if (left < right && top < bottom) {
+                for (int i = right - 1; i > left; i--) {
+                    ans[bottom][i] = num++;
+                }
+                for (int i = bottom; i > top; i--) {
+                    ans[i][left] = num++;
+                }
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
         }
-        return res;
+        return ans;
     }
 }
