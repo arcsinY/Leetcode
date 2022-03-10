e91329019cfa576ac4c86d71c10d532009dd7643
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-03-10 21:52:41 +0800

    Update 31.java

--- 字符串/31.java
+++ 字符串/31.java
@@ -1,39 +1,36 @@
-// 生成策略：找到一个小数和一个大数，交换位置，交换之后大数后面的数字要升序排列
+// 下一个排列的规则：
+// 1. 右边的一个大数与左边一个小数交换位置
+// 2. 右边的大数尽可能靠右
+// 3. 交换后大数后面的数字升序排列
 class Solution {
     public void nextPermutation(int[] nums) {
-        int i;int j=nums.length-1;
-        int t=nums[nums.length-1];
-        for(i=nums.length-1;i>=1;--i)  //找到后一个元素比前一个元素大的地方
-        {
-            if(nums[i]>nums[i-1])
-                break;
+        int i = nums.length - 2, j = nums.length - 1, k = nums.length - 1;
+        // 从右开始找出第一个 nums[i] < nums[i+1] 的位置
+        while (i >= 0 && nums[i] >= nums[j]) {
+            i--;
+            j--;
         }
-        if(i<1)  //已经是最大字典序了，直接反转
-        {
-            while(i<j)
-            {
-                t=nums[i];
-                nums[i]=nums[j];
-                nums[j]=t;
-                ++i;--j;
+        // 找右侧的大数
+        if (i >= 0) {
+            while (nums[k] <= nums[i]) {
+                k--;
             }
-            return;
+            swap(nums, i, k);
         }
-        int min_index=i;  //找到从nums[i-1]之后比nums[i-1]小的最大数，同时要求尽可能靠右
-        for(int k=i;k<= nums.length-1;++k)
-        {
-            if(nums[k]>nums[i-1] && nums[k]<=nums[min_index])
-                min_index=k;
-        }
-        t=nums[min_index];  //交换这两个数
-        nums[min_index]=nums[i-1];
-        nums[i-1]=t;
-        while(i<j)  //nums[i-1]之后的部分反转
-        {
-            t=nums[i];
-            nums[i]=nums[j];
-            nums[j]=t;
-            ++i;--j;
+        // 交换之后 j 右侧的数字降序排列，直接反转变成升序
+        k = nums.length - 1;
+        reverse(nums, i, k);
+    }
+    public void swap(int[] nums, int i, int k) {
+        int t = nums[i];
+        nums[i] = nums[k];
+        nums[k] = t;
+    }
+    public void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
         }
     }
 }
