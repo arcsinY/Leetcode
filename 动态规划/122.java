b92a3f7fcc982dd041578b181ac13c6d9897f3ec
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-03-27 16:53:36 +0800

    Update 122.java

--- 动态规划/122.java
+++ 动态规划/122.java
@@ -1,13 +1,16 @@
+// dp[i][0]：第 i 天手里没有股票，能够获得的最大利润
+// dp[i][1]：第 i 天手里有股票，能够获得的最大利润
+// dp[i][0] = max(dp[i-1][1]+prices[i], dp[i-1][0])，表示第 i 天手里没有股票可能是：1. 前一天手里有，今天卖掉了。2. 前一天没有，今天也没买
+// dp[i][1] = max(dp[i-1][0]-prices[i], dp[i-1][1])，表示第 i 天手里有股票可能是：1. 前一天手里没有，今天买了。2. 前一天有，今天也没卖
+// 将 dp[i][0] 转化为 dp0，dp[i][1] 转化为 dp1
 class Solution {
     public int maxProfit(int[] prices) {
-        int dp_i0 = 0, dp_i1 = Integer.MIN_VALUE;
-        for (int i = 0; i < prices.length; ++i) {
-            int t = dp_i0;
-            // 手里没有股票，可能是前一天也没有，可能是前一天有、今天卖了
-            dp_i0 = Math.max(dp_i0, dp_i1 + prices[i]);
-            // 手里有股票，可能是前一天就有，可能是刚买的
-            dp_i1 = Math.max(dp_i1, t - prices[i]);
+        int dp0 = 0, dp1 = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            int t = dp0;
+            dp0 = Math.max(dp0, dp1 + prices[i]);
+            dp1 = Math.max(t - prices[i], dp1);
         }
-        return dp_i0;
+        return dp0;
     }
 }
