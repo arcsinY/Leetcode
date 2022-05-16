2c4db8333a15615c3499097f3518ae47e8ee2674
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-05-16 14:23:34 +0800

    Update 239.java

--- 滑动窗口/239.java
+++ 滑动窗口/239.java
@@ -1,35 +1,23 @@
-// 双端队列：1. 队头是队列中最大元素
-//          2. 队列中元素从大到小有序
-//          3. 队列中元素都在窗口范围内（为了能够判断，队列中存的是下标）
-// 性质1保证了每次取出最大元素，性质2保证了性质1，性质3是题目要求
+// 单调队列，用双端队列实现
+// 入队时，从队尾开始判断，< 当前元素的从队尾移除
+// 出队时，判断队头是否等于当前元素
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> q = new LinkedList<>();
-        int left = 0, right = k - 1;
-        List<Integer> res = new ArrayList<>();
-        for (int i = left; i <= right; ++i) {
-            // 队列维护：队尾元素 < 当前元素的都出队
-            while (q.isEmpty() == false && nums[q.getLast()] < nums[i]) {
-                q.removeLast();
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> q;
+        for (int i = 0; i < nums.size(); i++) {
+            while (!q.empty() && q.back() < nums[i]) {
+                q.pop_back();
             }
-            q.addLast(i);
+            q.push_back(nums[i]);
+            if (i >= k - 1) {
+                ans.push_back(q.front());
+                if (!q.empty() && q.front() == nums[i - k + 1]) {
+                    q.pop_front();
+                }
+            }
         }
-        res.add(nums[q.getFirst()]);
-        ++right;
-        ++left;
-        while (right < nums.length) {
-            // 只需判断队头是否在窗口内，因为指针每次移动1位，队列不可能有两个元素都在窗口外
-            if (q.isEmpty() == false && q.getFirst() < left) {
-                q.removeFirst();
-            }
-            while (q.isEmpty() == false && nums[q.getLast()] < nums[right]) {
-                q.removeLast();
-            }
-            q.addLast(right);
-            res.add(nums[q.getFirst()]);
-            ++left;
-            ++right;
-        }
-        return res.stream().mapToInt(Integer::valueOf).toArray();
+        return ans;
     }
-}
+};
