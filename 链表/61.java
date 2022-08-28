c9df321d7c65f40da55a1ccc4b1427dbb238c34d
arcsinY <34400130+arcsinY@users.noreply.github.com>
2022-08-29 00:17:23 +0800

    Update 61.java

--- 链表/61.java
+++ 链表/61.java
@@ -1,21 +1,22 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null || k == 0)
+        if(head == null || head.next == null || k == 0) {
             return head;
+        }
         ListNode newHead = head;
         ListNode newTail = head;
         // 链表长度
         int n = 0;
-        while(head.next !=null)  //链表形成环，计算长度
-        {
+        while(head.next !=null) {
+            //链表形成环，计算长度
             ++n;
             head = head.next;
         }
         ++n;
         head.next = newHead;
         ListNode t = newHead;
-        for(int i = 1;i <= n- k%n; i++)  //找新链表的尾部
-        {
+        for(int i = 1;i <= n- k%n; i++) {
+            //找新链表的尾部
             t = t.next;
         }
         newTail = t;
